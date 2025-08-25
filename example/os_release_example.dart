91dd92cd05057f49f40f0155743aaf54ab933d66
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-25 10:53:18 +0100

    Update os_release_example.dart

--- example/os_release_example.dart
+++ example/os_release_example.dart
@@ -4,7 +4,7 @@ import 'package:os_release/os_release.dart';
 
 Future<void> main() async {
  if(Platform.isLinux){
-   var osRel = await OsRelease.detect();
+   var osRel = await LinuxOsRelease.detect();
 
    print("We're running: ${osRel.name}, which is a Linux distribution. That's so cool!");
  }
